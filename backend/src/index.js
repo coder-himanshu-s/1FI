65bd4d8db5488291c788bef7c0ec8621549fc5fc
Himanshu Sharma <himanshuindia2004@gmail.com>
2026-02-22 21:28:05 +0530

    Add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,10 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
